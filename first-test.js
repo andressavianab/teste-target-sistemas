67ffbe801161072d4f822192bef46e319bce27c3
andressavianab <andressavianab@gmail.com>
2025-01-10 19:46:11 -0300

    check lowest and highest turnover, and count days with turnover above the monthly average

--- first-test.js
+++ first-test.js
@@ -1,3 +1,5 @@
+import data from "./data.json" with { type: "json"};
+
 export function logSum() {
   let index = 13;
   let sum = 0;
@@ -25,4 +27,43 @@ export function isFibonacci(n) {
   }
 
   return `O número ${n} pertence à sequência de Fibonacci`;
+}
+
+export function lowestTurnover() {
+  let validTurnovers = data.filter((item) => item.turnover > 0);
+
+  let lowestTurnover = validTurnovers[0].turnover;
+  for (let i = 0; i < validTurnovers.length; i++) {
+    if (validTurnovers[i].turnover < lowestTurnover) {
+      lowestTurnover = validTurnovers[i].turnover;
+    }
+  }
+
+  return lowestTurnover;
+}
+
+export function highestTurnover() {
+  let validTurnovers = data.filter((item) => item.turnover > 0);
+
+  let highestTurnover = 0;
+
+  for (let i = 0; i < validTurnovers.length; i++) {
+    if (validTurnovers[i].turnover > highestTurnover) {
+        highestTurnover = validTurnovers[i].turnover;
+        }
+    }
+    return highestTurnover;
+}
+
+export function daysBiggerThanMonthAverage() {
+    let average = (data.reduce((acc, item) => acc + item.turnover, 0) / data.length).toFixed(2);
+
+    let days = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].turnover > average) {
+            days++;
+        }
+    }
+
+    return days;
 }
