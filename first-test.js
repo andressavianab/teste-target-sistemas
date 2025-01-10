42c19cd0ebfbe5255033854e6aeb6c622c7de90b
andressavianab <andressavianab@gmail.com>
2025-01-10 18:44:06 -0300

    check if a number isFibonacci sequence

--- first-test.js
+++ first-test.js
@@ -1,10 +1,28 @@
-let index = 13; 
-let sum = 0;
-let k = 0;
-
-while (k < index) {
+export function logSum() {
+  let index = 13;
+  let sum = 0;
+  let k = 0;
+  while (k < index) {
     k = k + 1;
     sum = sum + k;
+  }
+
+  console.log(sum);
 }
 
-console.log(sum);
+export function isFibonacci(n) {
+  let a = 0;
+  let b = 1;
+
+  while (a < n) {
+    let tempA = a;
+    a = a + b;
+    b = tempA;
+  }
+
+  if (a !== n) {
+    return `O número ${n} não pertence à sequência de Fibonacci`;
+  }
+
+  return `O número ${n} pertence à sequência de Fibonacci`;
+}
